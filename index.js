033ca32133002528ac0027894d2a7f140757624b
RimshaSaleem <rimshatif8@gmail.com>
2021-09-02 19:51:32 -0700

    wip

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require("dotenv").config()
 const express = require('express')
 const cors = require('cors')
 
-console.log(process.env.USER)
-console.log(process.env.HOME)
+ console.log(process.env.USER)
+ console.log(process.env.HOME)
 console.log(process.env.PORT)
+const PORT = process.env.PORT || 4000
 
 const server = express()
 
@@ -11,13 +13,17 @@ server.use(express.json())
 server.use(cors())
 
 server.get('/', (req, res) => {
-  res.send(`<h1>Web 45 ROCKS!</h1>`)
+  res.send(`<h1>Web 45 ROCKS!<br><br>
+   Rimsha</h1>`)
 })
 
 server.get('/api', (req, res) => {
   res.json({ message: 'Web 45 is awesome!' })
 })
+server.get('/api/:id', (req, res) => {
+    res.json({ message: 'Welcome to this new world' })
+  })
 
-server.listen(4000, () => {
-  console.log(`listening on port 4000`)
+server.listen(process.env.PORT, () => {
+  console.log(`listening on port ${PORT}`)
 })
